e6b452516cf8d22d78dacdbf9c53da7ae781de07
Noah Pan <noah.pan@classpass.com>
2021-06-07 21:12:08 -0400

    Add oh-my-zsh plugins

--- mac/brew.sh
+++ mac/brew.sh
@@ -67,3 +67,12 @@ brew cleanup
 # Install oh-my-zsh
 $ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+
+
